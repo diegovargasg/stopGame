01c07cafaf49c04539a85f6c42fb761174765038
diegovargasg <diegodevelopero@gmail.com>
2020-06-22 18:41:15 +0200

    SASS added for custom variables

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./styles/styles.css";
+import "./styles/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
